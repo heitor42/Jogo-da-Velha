355cda15fdfd32dee7f73d295a21eac326dfe642
Heitor <he1tor@hotmail.com>
2019-11-27 15:58:31 -0300

    Conclusão da parte que verifica quem ganhou.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Jogo da velha
 Primeiro projeto 
 Objetivo:
-- criar um jogo da velha que verifica quem ganhou e se deu velha
+* Criar um jogo da velha que verifica quem ganhou e se deu velha
 
 ## Metas
 - [X] Criar interface básica
-- [ ] Verifica quem ganhou
+- [X] Verifica quem ganhou
 - [ ] Verifica se deu velha
 - [ ] Verifica de quem é a vez
 - [ ] Melhorar interface do jogo
 
+
 - [ ] Evoluir um pouco o projeto implementando o jogo da velha 3D
-- Adiciona duas novas tabelas que representam as dimensões além de mudar um pouco a forma de jogar 
+* Adiciona duas novas tabelas que representam as dimensões além de mudar um pouco a forma de jogar 
--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-//var player1 = prompt("Player 1:")
-//var player2 = prompt("player 2:")
-var body = document.getElementById("gamebody")
-console.log(body)
-console.log('game work')
-
-function diegame(){
-    let diegame = []
+//var playerX = prompt("Player X:")
+//var playerO = prompt("player O:")
+function gamematrix(){
+    let gamematrix = []
     for (l=0; l<=2; l++){
         let linha = []
+
         for (c=0; c<=2; c++){
             linha.push(document.getElementById(`${l}${c}`).value)
         }
-        diegame[l] = linha
+        gamematrix[l] = linha
     }
-    return diegame
+    return gamematrix
 }
+
+function winner(){
+    let matrix = gamematrix()
+
+    //primeiro caso (linhas e colunas)
+    for (l=0; l<3; l++){
+        if (Boolean(matrix[l][l])){  
+            if (matrix[l][0]==matrix[l][1] && matrix[l][1]==matrix[l][2]){
+                return matrix[l][l]
+            }
+            if (matrix[0][l]==matrix[1][l] && matrix[0][l]==matrix[2][l]){
+                return matrix[l][l]
+            }
+        }
+    }
+
+    //segundo caso (diagonais)
+    if (Boolean(matrix[1][1])){
+        switch (true) {
+            case (matrix[1][1]==matrix[0][0] && matrix[1][1]==matrix[2][2]):
+                return matrix[1][1]
+                break;
+            case (matrix[1][1]==matrix[2][0] && matrix[1][1]==matrix[0][2]):
+                return matrix[1][1]
+                break;
+        }
+    }
+}
+
 function testa(){
-    console.log(diegame())
+    console.log(gamematrix())
+    console.log(winner())
 }
+
