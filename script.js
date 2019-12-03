6db33662b869fa8e4518014267b3fd18037ca8ef
Heitor <he1tor@hotmail.com>
2019-12-03 18:00:59 -0300

    Remoção dos alertas e adição de uma div para avisar quem ganhou ou se deu empate

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function winner(){
     for (i=0; i<=6; i+=3){
         if (Boolean(matrix[i])){
             if (matrix[i] == matrix[i+1] && matrix[i] == matrix[i+2]){
-                alert(`O jogador com ${matrix[i]} ganhou.`)
-                window.location.reload()
+                alerta(matrix[i])
             } 
         } 
     }
@@ -21,30 +20,44 @@ function winner(){
     for (i=0; i<=3; i++){
         if (Boolean(matrix[i])){
             if (matrix[i]==matrix[i+3] && matrix[i]==matrix[i+6]){
-                alert(`O jogador com ${matrix[i]} ganhou.`)
-                window.location.reload()
+                alerta(matrix[i])
             }
         }
     }
 
     if (Boolean(matrix[4])){
         if (matrix[4]==matrix[0] && matrix[4]==matrix[8]){
-            alert(`O jogador com ${matrix[4]} ganhou.`)
-            window.location.reload()
+            alerta(matrix[4])
         } else if (matrix[4]==matrix[2] && matrix[4]==matrix[6]){
-            alert(`O jogador com ${matrix[4]} ganhou.`)
-            window.location.reload()
+            alerta(matrix[4])
         }
     }
 }
 
 function DeuVelha() {
     let matrix = gamematrix()
-    matrix.sort()
-    if (matrix[0] == matrix[1] == Boolean(matrix[2])){
-        alert("Deu Velha")
-        window.location.reload()
+    let v = 0
+    for (i in matrix){
+        if (Boolean(matrix[i])){
+            v++
+        }
     }
+    if (v==9){
+        let p = document.createElement("P")
+        let t = document.createTextNode("Deu velha")
+        p.appendChild(t)
+        document.getElementById("div2").appendChild(p)
+        document.getElementById("div2").addEventListener("click", event =>{history.go(0)})
+        
+    }
+}
+
+function alerta(x) {
+    let p = document.createElement("P")
+    let t = document.createTextNode(`O jogador ${x} ganhou`)
+    p.appendChild(t)
+    document.getElementById("div2").appendChild(p)
+    document.getElementById("div2").addEventListener("click", event =>{history.go(0)})
 }
 
 function runnig() {
